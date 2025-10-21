8d85b701c22e957c07328757da4fb464f154decf
lukidu <sgb7r4mkv2@privaterelay.appleid.com>
2025-10-21 19:50:41 +0200

    V4.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const ADVENT_YEAR = 2025;       // Hier Jahr anpassen für nächsten Adventskalender
-const TEST_MODE = false;        // true = benutze TEST_DAY anstatt echtes Datum
+const TEST_MODE = true;        // true = benutze TEST_DAY anstatt echtes Datum
 const TEST_DAY = 2;             // bis zu welchem Tag testweise geöffnet werden darf
 
 const calendar = document.getElementById('calendar');
